cabd88b7e04b5c6bc6629c0b146a997e56eba44e
U-Nikhitha <uthoornikitha75785@gmail.com>
2023-04-20 19:21:20 +0530

    queryselector

---
+++ dom.js
@@ -0,0 +1,10 @@
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green';
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.textContent='  ';
+var secondItem=document.querySelectorAll('.list-group-item:nth-child(2)');
+items[0].fontColor='green';
+var odd=document.querySelectorAll('.li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
